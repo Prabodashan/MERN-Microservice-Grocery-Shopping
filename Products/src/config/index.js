9b59b7b48cfa74ae9e0c2fd016b4efebdea1f37e
Prabodashan <prabodashan14@gmail.com>
2023-03-25 17:04:44 +0530

    added product services

--- Products/src/config/index.js
+++ Products/src/config/index.js
@@ -1,11 +1,11 @@
 const dotEnv = require("dotenv");
 
-if (process.env.NODE_ENV !== "prod") {
-  const configFile = `./.env.dev`;
-  dotEnv.config({ path: configFile });
-} else {
-  dotEnv.config();
-}
+// if (process.env.NODE_ENV !== "prod") {
+//   const configFile = `./.env.dev`;
+//   dotEnv.config({ path: configFile });
+// } else {
+dotEnv.config();
+// }
 
 module.exports = {
   PORT: process.env.PORT,
